f43c5d85d6cc94a4c76184b966bf8489251c1065
Aleksandr <sanyasavva.ss@gmail.com>
2021-11-13 12:00:34 +0300

    App skeleton   * simple typescript component test

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app"> <Counter/></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Counter from './components/Counter.vue'
+
+export default defineComponent({
+  components: {
+    Counter
+  }
+})
+
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+  data () {
+    return {
+      valueNumber: 1
+    }
+  },
+
+  methods: {
+
+    getFormattedNumber (): string {
+      return `Your total number is ${this.valueNumber}`
+    },
+
+    increase (): void {
+      this.valueNumber += 1
+    },
+
+    decrease (): void {
+      this.valueNumber -= 1
+    }
+  }
+})
+
+</script>
+
+<template>
+  <div id='app'>
+    <fieldset>
+      <legend>{{ valueNumber }}</legend>
+      <button @click="increase"> Increase </button>
+      <button @click="decrease"> Decrease </button>
+    </fieldset>
+  </div>
+</template>
