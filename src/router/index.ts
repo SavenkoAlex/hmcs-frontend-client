74ea9da97c9c62b6e6ce4ade6dd0f96fe23bf8c9
Aleksandr <sanyasavva.ss@gmail.com>
2022-01-10 23:37:32 +0300

    basic templates views were deleted

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
 
+const home = {
+  template: '<div>home</div>'
+}
+
+const about = {
+  template: '<div>about</div>'
+}
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: home
   },
   {
     path: '/about',
@@ -13,7 +19,7 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: about
   }
 ]
 
