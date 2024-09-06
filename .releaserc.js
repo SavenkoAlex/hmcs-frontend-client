3c4b47035f8b3f481ed0b1d29572d0a5579a763a
Aleksandr <sanyasavva.ss@gmail.com>
2024-09-06 13:47:11 +0300

    feat(ci/cd): build app release

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,23 @@
 module.exports = {
-  branches: ['develop'],
+  private: true,
+  branches: ['master'],
   plugins: [
     '@semantic-release/commit-analyzer', 
     '@semantic-release/release-notes-generator', 
-    '@semantic-release/github'
-  ]
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+  ],
+  release: {
+    prepare: [
+      '@semantic-release/changelog',
+      {
+        "path": "@semantic-release/github",
+        "assets": [
+          { path: 'dist/assets/dist', label: 'frontend build' },
+          "CHANGELOG.md"
+        ],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  }
 }
