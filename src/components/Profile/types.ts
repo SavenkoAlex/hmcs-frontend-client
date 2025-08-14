1141658db6a4e9aa2e74623607c80403c5909238
Aleksandr <sanyasavva.ss@gmail.com>
2025-08-14 22:34:19 +0300

    feat: display amout feature added

--- src/components/Profile/types.ts
+++ src/components/Profile/types.ts
@@ -3,5 +3,8 @@ import { User } from '@/types/global'
 export type UserDataProfile = {
   userData: User | null,
   repeatPassword: string,
-  newPassword: string
+  newPassword: string,
+  plusIconPath: string,
 }
+
+export const MIN_AMOUNT = 100
