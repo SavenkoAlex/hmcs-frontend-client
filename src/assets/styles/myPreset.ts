58d6afb8fb329a9b4e5c5dd64d4d1b33a27bae95
Aleksandr <sanyasavva.ss@gmail.com>
2025-08-24 11:05:00 +0300

    fix: dark color theme chenged

--- src/assets/styles/myPreset.ts
+++ src/assets/styles/myPreset.ts
@@ -1,5 +1,8 @@
 import { definePreset } from '@primeuix/themes'
 import Aura from '@primeuix/themes/aura'
+import Material from '@primeuix/themes/material'
+import Lara from '@primeuix/themes/lara'
+import Nora from '@primeuix/themes/nora'
 
 export const MyPreset = definePreset(Aura, {
   components: {
@@ -10,28 +13,42 @@ export const MyPreset = definePreset(Aura, {
         paddingY: '.8rem'
       }
     },
-    button: {
-      root: {
-        sm: {
-          
-        }
-      }
-    }
   },
 
   semantic: {
-    primary: {
-      50: `{slate.50}`,
-      100: `{slate.100}`,
-      200: `{slate.200}`,
-      300: `{slate.300}`,
-      400: `{slate.400}`,
-      500: `{slate.500}`,
-      600: `{slate.600}`,
-      700: `{slate.700}`,
-      800: `{slate.800}`,
-      900: `{slate.900}`,
-      950: `{slate.950}`
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
+          50: `{slate.50}`,
+          100: `{slate.100}`,
+          200: `{slate.200}`,
+          300: `{slate.300}`,
+          400: `{slate.400}`,
+          500: `{slate.500}`,
+          600: `{slate.600}`,
+          700: `{slate.700}`,
+          800: `{slate.800}`,
+          900: `{slate.900}`,
+          950: `{slate.950}`
+        }
+      },
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: `{slate.50}`,
+          100: `{slate.100}`,
+          200: `{slate.200}`,
+          300: `{slate.300}`,
+          400: `{slate.400}`,
+          500: `{slate.500}`,
+          600: `{slate.600}`,
+          700: `{slate.700}`,
+          800: `{slate.800}`,
+          900: `{slate.900}`,
+          950: `{slate.950}`
+        }
+      }
     }
   }
 })
